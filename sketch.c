3a15b30f2b8d3e0667af11acf985a72e3e646e6b
matt-friend <mattfriend00@gmail.com>
2019-01-01 23:22:22 +0000

    started S2

--- sketch.c
+++ sketch.c
@@ -9,6 +9,9 @@ struct state{
 	int xpos;
 	int ypos;
 	int dx;
+	int DT;//last non-zero DT value
+	unsigned long operand; //operand under construction
+	bool valInit; //is operand initialised
 };
 
 typedef struct state state;
@@ -19,6 +22,9 @@ state *newState(){
 	s -> xpos = 0;
         s -> ypos = 0;
 	s -> dx = 0;
+	s -> DT = 0;
+	s -> operand = 0;
+	s -> valInit = false;
 	return s;
 }
 
@@ -52,17 +58,24 @@ void togglePen(state *s){
 	else s -> penMode = true;
 }
 
-void updateState(unsigned int b, state *s, display *d){
+void updateOperand(unsigned int b, state *s){
+	unsigned int newBits = b&0x2F;
+	s -> operand = s -> operand << 6 | newBits;
+}
+
+void update(unsigned int b, state *s, display *d){
 	int opc = getOpcode(b);
 	int opr = getOperand(b);
-	if (opc == 1 && s -> penMode == true) drawLine(s,d,opr);
-	else if (opc == 1) updatePos(s,opr);
-	else if (opc == 0) s -> dx = opr;
-	else if (opc == 3) togglePen(s);
-	else printf("opc = 2\n");
+	int longOp = 3;
+	if (opc == 0) s -> dx = opr;
+	else if (opc == 1){
+		if (s -> penMode == true) drawLine(s,d,opr);
+		else if (opc == 1) updatePos(s,opr);}
+	else if (opc == 2) updateOperand(b,s);
+	else longOp = longOp + opr;
 }	
 
-void test(){
+void testS1(){
 	assert(getOpcode(0x00)==0);
 	assert(getOpcode(0xC0)==3);
 	assert(getOperand(0x1F)==31);
@@ -75,8 +88,12 @@ void test(){
 	free(s);
 }
 
+void testS2(){
+
+}
+
 int main(int n, char *args[n]) {
-	if (n==1) test();
+	if (n==1) testS1();
 	else if (n!=2) {fprintf(stdout, "Use ./sketch filename\n"); exit(1); }
 	else{
 		FILE *inp = fopen(args[1],"rb");
@@ -84,7 +101,7 @@ int main(int n, char *args[n]) {
 		display *d = newDisplay(args[1], 200, 200);
 		state *s = newState();
 		while(!feof(inp)){
-	if (n!=2) {fprintf(stdout, "Use ./sketch filename\n"); exit(1); }
+			update(b,s,d);
 			b = fgetc(inp);
 		}
 		end(d);
